d57912166377b3eab7f299db6f00bd276f6a261b
mrxehmad <mrxehmad@naver.com>
2024-12-02 20:02:02 +0500

    Resume Url is still notfix

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "https://raw.githubusercontent.com/mrxehmad/mrxehmad.github.io/refs/heads/master/src/Assets/ehmi.pdf";
+import pdf from "../../Assets/../Assets/ehmi.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
