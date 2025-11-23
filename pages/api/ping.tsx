e7502c7098f27f942c1eb1567066de104f27866e
Ehmad Nobi <118040902+mrxehmad@users.noreply.github.com>
2025-11-23 13:28:42 +0500

    Add API endpoint for ping response

---
+++ pages/api/ping.tsx
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    res.status(200).json({ message: 'pong' });
+  } else {
+    res.setHeader('Allow', ['GET']);
+    res.status(405).json({ message: 'Method not allowed' });
+  }
+}
+
